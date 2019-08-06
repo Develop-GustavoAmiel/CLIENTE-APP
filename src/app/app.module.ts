a9d387524ab44e402a9c06180c8e8697c9f2e7de
Gustavo Amiel Urbina Avila <gustavoamielurbinaavila@MacBook-Pro-de-Gustavo.local>
2019-08-05 21:27:14 -0500

    Se crearon mas componentes y se registraron para poderlos utilizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {HeaderComponet} from './header/header.component';
+import { HeaderComponet } from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponet
+    HeaderComponet,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
